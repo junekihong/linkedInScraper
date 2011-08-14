96825296633cf6f95df0e19e759f945140f1b4e7
Juneki Hong <mynameisjunks@gmail.com>
2011-08-13 23:28:44 -0400

    Super basic stuff. the scraper looks at one specific page and takes a few things

--- linkedIn/linkedIn/items.py
+++ linkedIn/linkedIn/items.py
@@ -5,7 +5,7 @@
 
 from scrapy.item import Item, Field
 
-class LinkedinItem(Item):
+class linkedInItem(Item):
     # define the fields for your item here like:
     # name = Field()
     
--- linkedIn/linkedIn/spiders/linkedIn_spider.py
+++ linkedIn/linkedIn/spiders/linkedIn_spider.py
@@ -1,46 +1,52 @@
 from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+
+from linkedIn.items import linkedInItem
 
 class linkedInSpider(BaseSpider):
 	name = "linkedin.com"
 	allowed_domains = ["linkedin.com"]
 	start_urls = [
-		"http://www.linkedin.com/directory/people/a.html"#,
-     		#"http://www.linkedin.com/directory/people/b.html",
-                #"http://www.linkedin.com/directory/people/c.html",
-                #"http://www.linkedin.com/directory/people/d.html",
-                #"http://www.linkedin.com/directory/people/e.html",
-                #"http://www.linkedin.com/directory/people/f.html",
-                #"http://www.linkedin.com/directory/people/g.html",
-                #"http://www.linkedin.com/directory/people/h.html",
-                #"http://www.linkedin.com/directory/people/i.html",
-                #"http://www.linkedin.com/directory/people/j.html",
-                #"http://www.linkedin.com/directory/people/k.html",
-                #"http://www.linkedin.com/directory/people/l.html",
-                #"http://www.linkedin.com/directory/people/m.html",
-                #"http://www.linkedin.com/directory/people/n.html",
-                #"http://www.linkedin.com/directory/people/o.html",
-                #"http://www.linkedin.com/directory/people/p.html",
-                #"http://www.linkedin.com/directory/people/q.html",
-                #"http://www.linkedin.com/directory/people/r.html",
-                #"http://www.linkedin.com/directory/people/s.html",
-                #"http://www.linkedin.com/directory/people/t.html",
-                #"http://www.linkedin.com/directory/people/u.html",
-                #"http://www.linkedin.com/directory/people/v.html",
-                #"http://www.linkedin.com/directory/people/w.html",
-                #"http://www.linkedin.com/directory/people/x.html",
-                #"http://www.linkedin.com/directory/people/y.html",
-                #"http://www.linkedin.com/directory/people/z.html"
+		"http://www.linkedin.com/pub/rokalight-a-roka-holding-b-v-company/36/530/5b5"
+		#"http://www.linkedin.com/directory/people/as.html"
+
+
+		#"http://www.linkedin.com/directory/people/a.html",
+        #"http://www.linkedin.com/directory/people/b.html",
+        #"http://www.linkedin.com/directory/people/c.html",
+        #"http://www.linkedin.com/directory/people/d.html",
+        #"http://www.linkedin.com/directory/people/e.html",
+        #"http://www.linkedin.com/directory/people/f.html",
+        #"http://www.linkedin.com/directory/people/g.html",
+        #"http://www.linkedin.com/directory/people/h.html",
+        #"http://www.linkedin.com/directory/people/i.html",
+        #"http://www.linkedin.com/directory/people/j.html",
+        #"http://www.linkedin.com/directory/people/k.html",
+        #"http://www.linkedin.com/directory/people/l.html",
+        #"http://www.linkedin.com/directory/people/m.html",
+        #"http://www.linkedin.com/directory/people/n.html",
+        #"http://www.linkedin.com/directory/people/o.html",
+        #"http://www.linkedin.com/directory/people/p.html",
+        #"http://www.linkedin.com/directory/people/q.html",
+        #"http://www.linkedin.com/directory/people/r.html",
+        #"http://www.linkedin.com/directory/people/s.html",
+        #"http://www.linkedin.com/directory/people/t.html",
+        #"http://www.linkedin.com/directory/people/u.html",
+        #"http://www.linkedin.com/directory/people/v.html",
+        #"http://www.linkedin.com/directory/people/w.html",
+        #"http://www.linkedin.com/directory/people/x.html",
+        #"http://www.linkedin.com/directory/people/y.html",
+        #"http://www.linkedin.com/directory/people/z.html"
 	]
 
 	def parse(self, response):
-		#filename = response.url.split("/")[-2]
-		#open(filename, 'wb').write(response.body)
-		hxs = HtmlXPathSelector(response)
-		sites = hxs.select('//ul/li')
-		for site in sites:
-			sites2 = site.select('//ul/li').extract()
-			#print newSite
-			for site2 in sites2:
-				sites3 = site2.select('//ul/li').extract()
-				print newSite
+		item = linkedInItem()
+		hxs = HtmlXPathSelector(response)	   	
+		
+		item['name'] = hxs.select('//h1/span/span/text()').extract()
+		item['location'] = hxs.select('//dd/span/text()').extract()
+		item['industry'] = hxs.select('//dd[@class="industry"]/text()').extract()
+		
+		return item    
+	    
 
